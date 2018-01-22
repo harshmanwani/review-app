3fbcb5abb480fc23680da433a672e66192647b4c
harshmanwani <invokerharsh@gmail.com>
2018-01-22 19:45:38 +0530

    pages-structure-bootstrap

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+// const random = require("random-world")
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+let camps = [
+    { name: "Manali", image: "https://images.unsplash.com/photo-1503265192943-9d7eea6fc77a?auto=format&fit=crop&w=400&q=80" },
+    { name: "Shimla", image: "https://images.unsplash.com/photo-1437382944886-45a9f73d4158?auto=format&fit=crop&w=400&q=80" },
+    { name: "Kasol", image: "https://images.unsplash.com/photo-1496080174650-637e3f22fa03?auto=format&fit=crop&w=400&q=80" },
+    { name: "Manali", image: "https://images.unsplash.com/photo-1503265192943-9d7eea6fc77a?auto=format&fit=crop&w=400&q=80" },
+    { name: "Shimla", image: "https://images.unsplash.com/photo-1437382944886-45a9f73d4158?auto=format&fit=crop&w=400&q=80" },
+    { name: "Kasol", image: "https://images.unsplash.com/photo-1496080174650-637e3f22fa03?auto=format&fit=crop&w=400&q=80" },
+    { name: "Manali", image: "https://images.unsplash.com/photo-1503265192943-9d7eea6fc77a?auto=format&fit=crop&w=400&q=80" },
+    { name: "Shimla", image: "https://images.unsplash.com/photo-1437382944886-45a9f73d4158?auto=format&fit=crop&w=400&q=80" },
+    { name: "Kasol", image: "https://images.unsplash.com/photo-1496080174650-637e3f22fa03?auto=format&fit=crop&w=400&q=80" }
+];
 
 app.get("/", function(req, res){
     res.render("landing");
-})
+});
 
 app.get("/camps", function(req, res){
-    let camps = [
-        { name: "Manali", image: "https://images.unsplash.com/photo-1503265192943-9d7eea6fc77a?auto=format&fit=crop&w=400&q=80" },
-        { name: "Shimla", image: "https://images.unsplash.com/photo-1437382944886-45a9f73d4158?auto=format&fit=crop&w=400&q=80" },
-        { name: "Kasol", image: "https://images.unsplash.com/photo-1496080174650-637e3f22fa03?auto=format&fit=crop&w=400&q=80" }
-    ];
     res.render("camps", {camps});
-})
+});
+
+app.get("/camps/new", function(req, res){
+    res.render("newCamp");
+});
+
+app.post("/camps", function(req, res){
+   const name = req.body.name;
+   const image = req.body.image;
+   const newCamp = { name, image};
+   camps.push(newCamp);
+   res.redirect("/camps"); 
+});
 
 app.listen(3000, function(){
     console.log("Server started at port 3000");
