017b6254428303bc97134e3fcc83bd5d98245309
harshmanwani <invokerharsh@gmail.com>
2018-02-02 16:26:05 +0530

    Data Persistance
    Added mongodb support

--- app.js
+++ app.js
@@ -1,29 +1,60 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
+const   express     = require("express"),
+        app         = express(),
+        bodyParser  = require("body-parser"),
+        mongoose    = require("mongoose");
+        
 // const random = require("random-world")
 
+mongoose.connect("mongodb://localhost/review");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
-let camps = [
-    { name: "Manali", image: "https://images.unsplash.com/photo-1503265192943-9d7eea6fc77a?auto=format&fit=crop&w=400&q=80" },
-    { name: "Shimla", image: "https://images.unsplash.com/photo-1437382944886-45a9f73d4158?auto=format&fit=crop&w=400&q=80" },
-    { name: "Kasol", image: "https://images.unsplash.com/photo-1496080174650-637e3f22fa03?auto=format&fit=crop&w=400&q=80" },
-    { name: "Manali", image: "https://images.unsplash.com/photo-1503265192943-9d7eea6fc77a?auto=format&fit=crop&w=400&q=80" },
-    { name: "Shimla", image: "https://images.unsplash.com/photo-1437382944886-45a9f73d4158?auto=format&fit=crop&w=400&q=80" },
-    { name: "Kasol", image: "https://images.unsplash.com/photo-1496080174650-637e3f22fa03?auto=format&fit=crop&w=400&q=80" },
-    { name: "Manali", image: "https://images.unsplash.com/photo-1503265192943-9d7eea6fc77a?auto=format&fit=crop&w=400&q=80" },
-    { name: "Shimla", image: "https://images.unsplash.com/photo-1437382944886-45a9f73d4158?auto=format&fit=crop&w=400&q=80" },
-    { name: "Kasol", image: "https://images.unsplash.com/photo-1496080174650-637e3f22fa03?auto=format&fit=crop&w=400&q=80" }
-];
+const CampSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+const Camp = mongoose.model("Camp", CampSchema);
+
+// Camp.create(
+//     {
+//         name: "Manali",
+//         image: "https://images.unsplash.com/photo-1437382944886-45a9f73d4158?auto=format&fit=crop&w=400&q=80"
+//     }, 
+//     function(err, camp){
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log("New Camp Created");
+//             console.log(camp);
+//         }
+//     }
+// );
+
+// let camps = [
+//     { name: "Manali", image: "https://images.unsplash.com/photo-1503265192943-9d7eea6fc77a?auto=format&fit=crop&w=400&q=80" },
+//     { name: "Shimla", image: "https://images.unsplash.com/photo-1437382944886-45a9f73d4158?auto=format&fit=crop&w=400&q=80" },
+//     { name: "Kasol", image: "https://images.unsplash.com/photo-1496080174650-637e3f22fa03?auto=format&fit=crop&w=400&q=80" },
+//     { name: "Manali", image: "https://images.unsplash.com/photo-1503265192943-9d7eea6fc77a?auto=format&fit=crop&w=400&q=80" },
+//     { name: "Shimla", image: "https://images.unsplash.com/photo-1437382944886-45a9f73d4158?auto=format&fit=crop&w=400&q=80" },
+//     { name: "Kasol", image: "https://images.unsplash.com/photo-1496080174650-637e3f22fa03?auto=format&fit=crop&w=400&q=80" },
+//     { name: "Manali", image: "https://images.unsplash.com/photo-1503265192943-9d7eea6fc77a?auto=format&fit=crop&w=400&q=80" },
+//     { name: "Shimla", image: "https://images.unsplash.com/photo-1437382944886-45a9f73d4158?auto=format&fit=crop&w=400&q=80" },
+//     { name: "Kasol", image: "https://images.unsplash.com/photo-1496080174650-637e3f22fa03?auto=format&fit=crop&w=400&q=80" }
+// ];
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/camps", function(req, res){
-    res.render("camps", {camps});
+    Camp.find({}, function(err, allCampgrounds) {
+        if(err){
+            console.log(err);
+        } else {
+            res.render("camps", {camps : allCampgrounds});
+        }
+    })
 });
 
 app.get("/camps/new", function(req, res){
@@ -31,11 +62,17 @@ app.get("/camps/new", function(req, res){
 });
 
 app.post("/camps", function(req, res){
-   const name = req.body.name;
-   const image = req.body.image;
-   const newCamp = { name, image};
-   camps.push(newCamp);
-   res.redirect("/camps"); 
+    const name = req.body.name;
+    const image = req.body.image;
+    const newCamp = { name, image};
+    //    camps.push(newCamp);
+    Camp.create(newCamp, function(err, newCamps) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/camps"); 
+        }
+    })
 });
 
 app.listen(3000, function(){
